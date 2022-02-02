fb9db188812702fc796a9d35970f976d494da66d
JAISON <j.pathrose@gordian.com>
2022-02-03 01:25:50 +0530

    - Screenshots

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, flash
 from werkzeug.utils import secure_filename
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import f1_score, matthews_corrcoef
+from sklearn.metrics import confusion_matrix
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import mpld3
 ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
 def allowed_file(filename):
@@ -37,11 +46,42 @@ def success():
     outlier_fraction = len(Fraud)/float(len(Valid))
     fraudcases = 'Fraud Cases: {}'.format(len(data[data['Class'] == 1]))
     validcases = 'Valid Transactions: {}'.format(len(data[data['Class'] == 0]))
-    # result = {
-    #   Fraud:Fraud,
-    #   Valid:Valid
-    # }
-    return render_template('success.html', fraudcases=fraudcases, validcases=validcases)
+    fraudAmount = Fraud.Amount
+    validAmount = Valid.Amount
+    corrmat = data.corr()
+    fig = plt.figure(figsize = (5, 5))
+    sns.heatmap(corrmat, vmax = .8, square = True)
+    plot = mpld3.fig_to_html(fig)
+    X = data.drop(['Class'], axis = 1)
+    Y = data["Class"]
+    xData = X.values
+    yData = Y.values
+    xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.2, random_state = 42)
+    # random forest model creation
+    rfc = RandomForestClassifier()
+    rfc.fit(xTrain, yTrain)
+    # predictions
+    yPred = rfc.predict(xTest)
+    acc = format(accuracy_score(yTest, yPred))
+    prec = format(precision_score(yTest, yPred))
+    rec = format(recall_score(yTest, yPred))
+    f1 = format(f1_score(yTest, yPred))
+    MCC = format(matthews_corrcoef(yTest, yPred))
+    LABELS = ['Normal', 'Fraud']
+    conf_matrix = confusion_matrix(yTest, yPred)
+    fig1 = plt.figure(figsize =(5, 5))
+    sns.heatmap(conf_matrix, xticklabels = LABELS,
+                yticklabels = LABELS, annot = True, fmt ="d")
+    plt.title("Confusion matrix")
+    plt.ylabel('True class')
+    plt.xlabel('Predicted class')
+    plot1 = mpld3.fig_to_html(fig1)
+    
+    return render_template('success.html', 
+    fraudcases=fraudcases, validcases=validcases, 
+    outlier_fraction=outlier_fraction, fraudAmount=fraudAmount, 
+    validAmount=validAmount, plot=plot, acc=acc, prec=prec,
+    rec=rec, f1=f1, MCC=MCC, plot1=plot1)
   return "NULL"
 if __name__ == '__main__':
    app.run()
